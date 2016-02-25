8e92fbcced63874ccaba78457c8e8a7a8add932d
henanmyc <2500731882@qq.com>
2016-02-25 20:14:27 +0800

    Update HelloWorld.java

--- src/com/atguigu/hello/HelloWorld.java
+++ src/com/atguigu/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package com.atguigu.hello;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("hello client");
+		System.out.println("lient");
 		System.out.println("Hello Github");
 		System.out.println("client makes diff");
 	}
