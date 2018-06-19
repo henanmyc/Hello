ea4675109d6e965b236e9dff0143f90ca1085f35
unknown <miyc@sinosure.com.cn>
2018-06-19 14:54:39 +0800

    提交

--- src/com/atguigu/hello/HelloWorld.java
+++ src/com/atguigu/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package com.atguigu.hello;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("She is my sin");
+		System.out.println("She is my sintest");
 		System.out.println("lient");
 		System.out.println("Hello Github");
 		System.out.println("client makes diff");
