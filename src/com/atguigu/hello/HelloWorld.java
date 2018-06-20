82eff70cea90847cc9b179d673730baaf534e055
henanmyc <2500731882@qq.com>
2018-06-20 09:10:18 +0800

    Update HelloWorld.java

--- src/com/atguigu/hello/HelloWorld.java
+++ src/com/atguigu/hello/HelloWorld.java
@@ -7,9 +7,9 @@ public class HelloWorld {
 		System.out.println("lient");
 		System.out.println("Hello Github");
 		System.out.println("client makes diff");
-		System.out.println("45678");
 		System.out.println("master");
-		System.out.println("123");
+		System.out.println("master");
+		System.out.println("master");
 		System.out.println("master");
 	}
 
