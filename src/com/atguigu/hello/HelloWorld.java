08d501ff1706599b7861c801f3885fec94380c9c
unknown <henanmyc@163.com>
2018-06-21 15:35:14 +0800

    123

--- src/com/atguigu/hello/HelloWorld.java
+++ src/com/atguigu/hello/HelloWorld.java
@@ -7,10 +7,6 @@ public class HelloWorld {
 		System.out.println("lient");
 		System.out.println("Hello Github");
 		System.out.println("client makes diff");
-		System.out.println("test");
-		System.out.println("test");
-		System.out.println("test");
-		System.out.println("test");
 		System.out.println("master");
 		System.out.println("master");
 		System.out.println("master");
